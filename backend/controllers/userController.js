a412f3ff41906dccc39703bd1a5b6e2cc50b1158
Paul <freemeonline@gmail.com>
2024-04-05 17:20:06 -0500

    Added password hashing complete user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User successful" });
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
 });
 const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login Usersuccessful" });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  name: { type: String, required: [true, "Name is required"] },
-  email: { type: String, required: [true, "Email is required"], unique: true },
-  password: { type: String, required: [true, "Password is required"] },
-}{
-    timestamp: true
-});
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: [true, "Name is required"] },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: { type: String, required: [true, "Password is required"] },
+  },
+  {
+    timestamp: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
