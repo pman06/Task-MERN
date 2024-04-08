37241bb7438761bf4d4583deeb08ba37f9ed5e48
Paul <freemeonline@gmail.com>
2024-04-08 03:14:18 -0500

    Added JWT token generation on register and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,36 +3,65 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 
+// Create user registration
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
+  //throw error if missing name email and password
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
 
+  //   check database if user email already exists or throw error if so
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User Exists");
   }
 
+  //   create password hashing with salt
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
+  //   create user account with email and hashed password
   const user = await User.create({ name, email, password: hashedPassword });
 
+  //   IF created successfully, return the created user or throw error if not
   if (user) {
-    res.status(201).json({ _id: user.id, name: user.name, email: user.email });
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateJWTtoken(user.id),
+    });
   } else {
     res.status(400);
     throw new Error("Invalid User Data");
   }
 });
+
+// create log in function for users
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login Usersuccessful" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateJWTtoken(user.id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Password or email is invalid!");
+  }
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
   res.json({ message: "Current user Data" });
 });
+
+const generateJWTtoken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: `5d` });
 
 module.exports = { registerUser, loginUser, getCurrentUser };
