d8f63eee7343358e7bbcb4a63d65b378ce33d52a
Paul <freemeonline@gmail.com>
2024-04-12 17:33:55 -0500

    Added frontend start up template and cleanup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
